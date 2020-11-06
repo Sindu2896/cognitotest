d88e3201fa255388fd121b64494002ee2241ed38
Sindu2896 <sindujan.krishnamoorthy@insighture.com>
2020-11-06 12:24:46 +0530

    sample cognito hostedui

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Amplify, { Auth, Hub, API } from 'aws-amplify'
+import awsconfig from './awsconfig'
+import awsauth from './awsauth'
+import Dashboard from './dashboard'
+// const App = () => (
+//   <div>
+//     <AmplifySignOut />
+//     My App
+//   </div>
+// );
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {}
+    }
+    componentDidMount () {
+        Amplify.configure(awsconfig)
+        Auth.configure({ oauth: awsauth }) 
+        
+        Hub.listen('auth', ({ payload: { event, data } }) => {
+          switch (event) {
+            case 'signIn':
+              console.log('sign in', event, data)
+              this.setState({ user: data})
+              break
+            case 'signOut':
+              console.log('sign out')
+              this.setState({ user: null })
+              break
+          }
+        })
+    }
+    render() {
+        return (
+        <>
+        <button onClick={() => Auth.federatedSignIn()}>Open Hosted  email UI</button>
+        <button onClick={() => Auth.federatedSignIn({provider: "Google"})}>Open Hosted google UI</button>
+        {(this.state.user) ? <Dashboard />:<p>not logged in</p>}
+        <button onClick ={() => Auth.signOut()}>Logout</button>
+        </>
+        )
+    }
 }
 
 export default App;
---
+++ src/dashboard.js
@@ -0,0 +1,9 @@
+import { render } from '@testing-library/react'
+import React from 'react'
+
+const Dashboard = () => (
+
+     <p>Dasshboard</p>
+)
+
+export default Dashboard
