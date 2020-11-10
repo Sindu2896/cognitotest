d9d156acb41866e2218415b8feb2afbd4a1c96af
Sindu2896 <sindujan.krishnamoorthy@insighture.com>
2020-11-10 08:47:50 +0530

    functional

--- src/App.js
+++ src/App.js
@@ -1,47 +1,48 @@
-import React from 'react';
-import Amplify, { Auth, Hub, API } from 'aws-amplify'
-import awsconfig from './awsconfig'
-import awsauth from './awsauth'
-import Dashboard from './dashboard'
-// const App = () => (
-//   <div>
-//     <AmplifySignOut />
-//     My App
-//   </div>
-// );
+import React, { useEffect, useState } from 'react';
+import Amplify, { Auth, Hub } from 'aws-amplify';
+import awsconfig from './aws-exports';
 
-class App extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {}
-    }
-    componentDidMount () {
-        Amplify.configure(awsconfig)
-        Auth.configure({ oauth: awsauth }) 
-        
-        Hub.listen('auth', ({ payload: { event, data } }) => {
-          switch (event) {
-            case 'signIn':
-              console.log('sign in', event, data)
-              this.setState({ user: data})
-              break
-            case 'signOut':
-              console.log('sign out')
-              this.setState({ user: null })
-              break
-          }
-        })
-    }
-    render() {
-        return (
-        <>
-        <button onClick={() => Auth.federatedSignIn()}>Open Hosted  email UI</button>
-        <button onClick={() => Auth.federatedSignIn({provider: "Google"})}>Open Hosted google UI</button>
-        {(this.state.user) ? <Dashboard />:<p>not logged in</p>}
-        <button onClick ={() => Auth.signOut()}>Logout</button>
-        </>
-        )
-    }
+Amplify.configure(awsconfig);
+
+function App() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    Hub.listen('auth', ({ payload: { event, data } }) => {
+      switch (event) {
+        case 'signIn':
+        case 'cognitoHostedUI':
+          getUser().then(userData => setUser(userData));
+          break;
+        case 'signOut':
+          setUser(null);
+          break;
+        case 'signIn_failure':
+        case 'cognitoHostedUI_failure':
+          console.log('Sign in failure', data);
+          break;
+      }
+    });
+
+    getUser().then(userData => setUser(userData));
+  }, []);
+
+  function getUser() {
+    return Auth.currentAuthenticatedUser()
+      .then(userData => userData)
+      .catch(() => console.log('Not signed in'));
+  }
+
+  return (
+    <div>
+      <p>User: {user ? JSON.stringify(user.attributes) : 'None'}</p>
+      {user ? (
+        <button onClick={() => Auth.signOut()}>Sign Out</button>
+      ) : (
+        <button onClick={() => Auth.federatedSignIn()}>Federated Sign In</button>
+      )}
+    </div>
+  );
 }
 
 export default App;
